d0967d132b45473068ba3f9eefe60d37ab47658d
giagra91 <giagra91@gmail.com>
2022-03-11 13:42:45 +0100

    completamento snack A D

---
+++ snack-A/main.js
@@ -0,0 +1,31 @@
+const player = {
+    nome: `James`,
+    cognome: `Le Bron`,
+    età: 30,
+    mediaPunti: ``,
+    percSuccesso: ``,
+    codGiocatore: ``,    
+}
+
+let playerCode = "";
+
+const letters = `ABCDEFGHIJKLMNOPQRSTUVZ`;
+const numbers = `01233456789`;
+for (let i = 0; i < 3; i++){
+    playerCode += letters.charAt(Math.floor(Math.random() * letters.length));
+    playerCode += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    
+}
+
+player["codGiocatore"] = playerCode;
+console.log(playerCode)
+
+let mediaPoints = (Math.floor(Math.random() * 50));
+player["mediaPunti"] = mediaPoints;
+console.log(mediaPoints)
+
+let successPercentage = (Math.floor(Math.random() * 100));
+player["percSuccesso"] = successPercentage;
+console.log(successPercentage)
+
+console.log(player)
---
+++ snack-D/main.js
@@ -0,0 +1,33 @@
+const animals =    [
+{ 
+    nome: 'leone',
+    famiglia: 'felidi', 
+    classe: 'mammiferi' 
+},
+{ 
+    nome: 'cane', 
+    famiglia: 'canidi', 
+    classe: 'mammiferi' 
+},
+{ 
+    nome: 'gallina', 
+    famiglia: 'fasianidi', 
+    classe: 'uccelli' 
+},
+{ 
+    nome: 'gatto', 
+    famiglia: 'felini', 
+    classe: 'mammiferi' 
+},
+{ 
+    nome: 'orso', 
+    famiglia: 'prova', 
+    classe: 'mammiferi' 
+},
+];
+
+const newAnimals = animals.filter (element => {
+    if (element.classe == `mammiferi`)
+    return true;
+});
+console.table(newAnimals)
